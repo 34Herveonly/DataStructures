6c55fa7e4e59c4b88faa5e2dc155489a0a6d01c4
onlyherve34 <herveonly34@gmail.com>
2025-05-06 17:57:45 +0200

    created a logic to delete a value from a specified postion of an  array

---
+++ DeletingFromArray.c
@@ -0,0 +1,33 @@
+// This is a Dsa logic to delete a value from specified position of an array
+
+#include <stdio.h>
+
+int main (){
+    int size,arr[50],pos;
+    printf("Enter the size of array: ");
+    scanf("%d",&size);
+    
+    printf("Enter the values of array:");
+    for(int i=0; i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("The entered values are: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    printf("\nEnter the position you want to delete from ");
+    scanf("%d",&pos);
+    
+    for(int i=pos-1;i<size;i++){
+        arr[i]=arr[i+1];
+    }
+    size--;
+    
+    printf("The updated array is :");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
