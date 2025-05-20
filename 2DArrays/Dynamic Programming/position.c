738d26cb20a94f79938095e0344be65ce0fe0e21
onlyherve34 <herveonly34@gmail.com>
2025-05-20 16:35:12 +0200

    started to learn about dynamic programming

---
+++ 2DArrays/Dynamic Programming/position.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+int n,fib[50];
+printf("Enter the maximum value of sequence : ");
+    scanf("%d",&n);
+if(n>50){
+    printf("Error, The length cant exceed 50. ");
+    }
+else {
+
+fib[0]=0;
+fib[1]=1;
+// printf("Enter the values of our sequence");
+for(int i=2;i<=n;i++){
+    // scanf("%d",&fib[i]);
+    fib[i]=fib[i-1]+fib[i-2];
+}
+printf("The value at position n is :%d",fib[n]);
+    return 0;
+}
+}
