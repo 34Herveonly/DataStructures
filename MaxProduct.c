3875af6dba48634cbe4b73dd138f777b441dfb6d
onlyherve34 <herveonly34@gmail.com>
2025-07-03 13:12:48 +0200

    trying to tackle a 1d array leetcode kinda question

---
+++ MaxProduct.c
@@ -0,0 +1,3 @@
+//  Problem Title: Maximum Product Subarray
+
+/*Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.*/
