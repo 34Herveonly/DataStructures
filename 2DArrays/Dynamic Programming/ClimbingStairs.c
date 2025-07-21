3a4daaa3b80a91b5a662cc7afaf6b57f5b6db1d5
onlyherve34 <herveonly34@gmail.com>
2025-07-21 13:41:20 +0200

    just went through this question as a form of revision of how dynamic programming works

--- 2DArrays/Dynamic Programming/ClimbingStairs.c
+++ 2DArrays/Dynamic Programming/ClimbingStairs.c
@@ -20,3 +20,34 @@ int main() {
     }
     return 0;
 }
+/*
+
+// This was a revision of how we did this question as well as strenghtening the core understanding of how it works 
+
+// The newly learnt thing is if we had 5 as input and we're getting the stairs[4]+stairs[3] we dont add the 4 and 3 as the results of our computation we just add the previously obtained ways to reach them. for our case we would add the number of ways to reach the staie 4 and the stair 3 and we would be set.
+
+#include <stdio.h>
+
+int main() {
+    int stairs[20],pos;
+    
+    int size=sizeof(stairs)/sizeof(stairs[0]);
+    
+    printf("Enter the number of stairs to climb: ");
+    scanf("%d",&pos);
+    
+    if(pos>20){
+        printf("The staircase cant exceed 20 for it cant be computed");
+    }
+    else{
+        stairs[0]=1;
+        stairs[1]=1;
+    }
+    for(int i=2;i<=size;i++){
+        stairs[i]=stairs[i-1]+stairs[i-2];
+    }
+    printf("You can reach the stair %d in: %d ways",pos,stairs[pos]);
+    return 0;
+}
+
+*/ 
