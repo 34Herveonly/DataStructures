fc20a59df66f29f70f3cb4e690dc94843c6f4474
onlyherve34 <herveonly34@gmail.com>
2025-08-04 18:35:44 +0200

    initial commit on 567 leetcode question

---
+++ 2DArrays/Leetcode Questions/StringPermutation.c
@@ -0,0 +1,8 @@
+/*  Leetcode #567 - Permutation in String
+
+  Check if one string is a permutation of a substring of another.
+
+  classic fixed-size sliding window + hash maps.
+  
+  [Difficulty: Medium]
+*/
