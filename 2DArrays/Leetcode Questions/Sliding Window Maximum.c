c6253ceaacf24045a78cb294c59351d0fe018f9a
onlyherve34 <herveonly34@gmail.com>
2025-08-13 12:05:25 +0200

    initial commit on the 239 leetcode question

---
+++ 2DArrays/Leetcode Questions/Sliding Window Maximum.c
@@ -0,0 +1,13 @@
+/*
+Leetcode #239 - Sliding Window Maximum
+Different twist: focuses on numerical values instead of substrings.
+
+Description:
+
+You are given an array of integers nums, and an integer k. Return the maximum value in every window of size k.
+
+Why it's hard:
+
+You need an efficient data structure (like a deque) to maintain the max in O(n) time.
+*/
+
