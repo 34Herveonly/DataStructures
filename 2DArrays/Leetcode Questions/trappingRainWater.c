639023bde07cf6d9f4596e5fcd12f5547adfa5c6
onlyherve34 <herveonly34@gmail.com>
2025-08-27 05:59:46 +0200

    initial commit on the leetcode 42 question of 2 pointers category and its of hard category

---
+++ 2DArrays/Leetcode Questions/trappingRainWater.c
@@ -0,0 +1,12 @@
+/*
+Leetcode #42 - Trapping Rain Water
+Different twist: Simulates physical intuition using two pointers and pre-processing.
+
+Description:
+
+Given n non-negative integers representing an elevation map, compute how much water it can trap after raining.
+
+Why it's hard:
+
+Needs careful use of two pointers or precomputed max arrays to achieve O(n) time and O(1)/O(n) space.
+*/
